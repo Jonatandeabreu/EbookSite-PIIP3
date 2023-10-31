ff6acb11e9d8015675e10170b7e65cab06e8f5d1
Jonatandeabreu <jonatandeabreu95@gmail.com>
2023-10-31 10:51:46 -0300

    se agrega en todas las rutas validador de id

--- routes/libros.js
+++ routes/libros.js
@@ -2,14 +2,14 @@ const express = require('express');
 
 const { getTodos, agregar, editar, borrar, getByIDBook } = require('../controllers/libros')
 const { validarJWT, esAdmin } = require('../middleware/validar-rol')
-const { validateaddbook } = require('../middleware/validar_entrada');
+const { validateaddbook,validateID } = require('../middleware/validar_entrada');
 
 const router = express.Router();
 
 router.get('/', getTodos);
-router.get('/:id', getByIDBook);
+router.get('/:id',validateID, getByIDBook);
 router.post('/', validarJWT, esAdmin, validateaddbook, agregar);
-router.put('/:id', validarJWT, esAdmin, editar);
-router.delete('/:id', validarJWT, esAdmin, borrar);
+router.put('/:id', validateID,validarJWT, esAdmin, editar);
+router.delete('/:id', validateID,validarJWT, esAdmin, borrar);
 
 module.exports = router;
