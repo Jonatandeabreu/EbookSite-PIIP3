ebc9152782229b4c2a5e8e9ff2a4f03d8cf94c48
Jonatandeabreu <jonatandeabreu95@gmail.com>
2023-10-31 10:34:05 -0300

    se corrige autores

--- controllers/autores.js
+++ controllers/autores.js
@@ -1,45 +1,49 @@
 //const autoresFake = require("../data/authors")
-const autores = require('../models/authors');
-const libros = require('../models/Books');
+const autores = require("../models/authors");
+const libros = require("../models/Books");
 
 //obtener todos
 const getTodos = async (req, res) => {
-    const autor = await autores.find().exec();
-    res.json(autor);
-}
+  const autor = await autores.find().exec();
+  res.json(autor);
+};
 
 //obtener por nombre de autor (mayusucla y minusculas)
 const getByname = async (req, res) => {
-    let { nombre } = req.body;
-    const regex = new RegExp(nombre,'i')
-    const autor = await autores.find({ nombre:{$regex:regex}}).exec();
+  let { nombre } = req.body;
+  const regex = new RegExp(nombre, "i");
+  const autor = await autores.find({ nombre: { $regex: regex } }).exec();
 
-    if (autor.length > 0) {
-        res.json(autor);
-    } else {
-        res.status(404).json({
-            nombre,
-            encontrado: false,
-            msg: "No se encontro un autor con ese nombre"
-        })
-    }
-}
+  if (autor.length > 0) {
+    res.json(autor);
+  } else {
+    res.status(404).json({
+      nombre,
+      encontrado: false,
+      msg: "No se encontro un autor con ese nombre",
+    });
+  }
+};
 
 //obtener libros por ID de autor
 const getByID = async (req, res) => {
-    let { id } = req.params;
+  let { id } = req.params;
+  try {
     const libroEncontrado = await libros.find({ id_autor: id }).exec();
     if (libroEncontrado.length > 0) {
-        res.json(libroEncontrado);
+      res.json(libroEncontrado);
     } else {
-        res.json({
-            msg: "No se encontro libro con ese ID de autor"
-        })
+      res.json({
+        msg: "No se encontro libro con ese ID de autor",
+      });
     }
+  } catch (e) {
+    res.json(e);
+  }
 };
 
 module.exports = {
-    getTodos,
-    getByname,
-    getByID
-}
+  getTodos,
+  getByname,
+  getByID,
+};
