6dc1f3646c735412bacfc90e80468eb5266ba3ca
jonatan <jonatandeabreu95@gmail.com>
2023-11-01 11:39:28 -0300

    se agrega mensaje a validar_cali

--- middleware/validar_cali.js
+++ middleware/validar_cali.js
@@ -2,7 +2,7 @@
 const { validationResult, param, check } = require('express-validator');
 
 const validateCali = [
-  check('Calificación').isNumeric().isFloat({ min: 0, max: 5 }),
+  check('Calificación').isNumeric().isFloat({ min: 0, max: 5 }).withMessage("La calificación debe ser entre 0 y 5"),
   (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
